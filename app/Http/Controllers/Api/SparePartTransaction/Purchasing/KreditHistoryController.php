5d6157cf66381a9c064112726049e6e5873d2d6e
pomo heri <pomoheri@gmail.com>
2023-10-25 09:23:14 +0700

    update beberapa

--- app/Http/Controllers/Api/SparePartTransaction/Purchasing/KreditHistoryController.php
+++ app/Http/Controllers/Api/SparePartTransaction/Purchasing/KreditHistoryController.php
@@ -16,10 +16,15 @@ class KreditHistoryController extends Controller
     public function kreditHistory($transaction_unique)
     {
         try {
+            $balance = CreditPayment::where('transaction_unique', $transaction_unique)->orderBy('created_at', 'desc')->first();
             $data = CreditPayment::select('date','total as total_payment','amount','balance','remark')
                                 ->where('transaction_unique', $transaction_unique)
-                                ->orderBy('created_at', 'asc')
+                                ->orderBy('created_at', 'desc')
                                 ->get();
+            $data = [
+                'last_balance' => ($balance) ? $balance->balance : 0,
+                'data'         => $data,
+            ];
 
             return (new \App\Helpers\GlobalResponseHelper())->sendResponse($data, ['List Data Kredit History']);
         } catch (\Exception $e) {
