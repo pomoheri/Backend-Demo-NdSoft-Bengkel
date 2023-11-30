150b95cf4decca8e5a235a853a4c15b7e7fa4d58
pomo heri <pomoheri@gmail.com>
2023-11-30 10:27:26 +0700

    update rest laporan

--- app/Http/Controllers/Api/Laporan/LaporanController.php
+++ app/Http/Controllers/Api/Laporan/LaporanController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers\Api\Laporan;
 
-use App\Models\SmallTransaction;
+use Carbon\Carbon;
 use App\Models\WorkOrder;
+use DateTime;
 use Illuminate\Http\Request;
 use App\Models\PurchaseOrder;
 use App\Models\SellSparepart;
+use App\Models\SmallTransaction;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 
 class LaporanController extends Controller
@@ -66,13 +69,54 @@ class LaporanController extends Controller
             $buy_sparepart = PurchaseOrder::where('status', 'Paid')->whereMonth('closed_at',$month)->whereYear('closed_at', $year)->get()->sum('total');
             $total_cost = SmallTransaction::where('status', 'Kredit')->whereMonth('created_at',$month)->whereYear('created_at', $year)->where('category','Cost')->get()->sum('total');
 
+            //Pembelian Spare Part 3 Bulan Terakhir
+            $start_date_before_parse = Carbon::now()->startOfMonth()->subMonth(3)->format('Y-m-d');
+            $end_date_before_parse = Carbon::now()->endOfMonth()->format('Y-m-d'); 
+            $start_date = Carbon::parse($start_date_before_parse);
+            $end_date = Carbon::parse($end_date_before_parse);
+
+            $part_3_month = SellSparepart::select(DB::raw('YEAR(closed_at) year, MONTH(closed_at) month, SUM(total) total'))
+                                        ->where('status', 'Closed')
+                                        ->whereBetween('closed_at', [$start_date, $end_date])
+                                        ->groupBy('year', 'month')
+                                        ->get();
+
+            $list_3_month = [];
+
+            for ($i = 1; $i <= 3; $i++) {
+                $current_date = $start_date->copy()->addMonths($i);
+                $year = $current_date->year;
+                $month = $current_date->month;
+
+                $found = $part_3_month->first(function ($item) use ($year, $month) {
+                    return $item->year == $year && $item->month == $month;
+                });
+                $month_val = Carbon::createFromDate($year, $month, 1)->format('F');
+
+                if ($found) {
+                    $list_3_month[] = [
+                        'year' => $found->year,
+                        'month' => $month_val,
+                        'total' => $found->total
+                    ];
+                } else {
+                    $list_3_month[] = [
+                        'year' => $year,
+                        'month' => $month_val,
+                        'total' => 0
+                    ];
+                }
+            }
+
+
             $output = [
                 'jasa_service'   => $sum_jasa_service,
                 'total_sublet'   => $sum_sublet,
                 'sell_sparepart' => $sell_sparepart+$sum_part,
                 'buy_sparepart'  => $buy_sparepart,
                 'total_cost'     => $total_cost,
-                'profit_part'    => 'Profit Nilai nya masih belum fix, bimbang'
+                'profit_part'    => 'Profit Nilai nya masih belum fix, bimbang',
+                'buy_part_3_month'   => $list_3_month
             ];
 
             return (new \App\Helpers\GlobalResponseHelper())->sendResponse($output, ['Data Laporan']);
